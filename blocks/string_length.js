365a4e545ad0a0d8c5edd132d1316780fe64ca65
Zody Woolsey@greenbush <52427035+zwoolsey-gbush@users.noreply.github.com>
2019-10-28 08:50:21 -0500

    started Blockly

---
+++ blocks/string_length.js
@@ -0,0 +1,18 @@
+Blockly.Blocks['string_length'] = {
+  init: function() {
+    this.jsonInit({
+      "message0": 'length of %1',
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "VALUE",
+          "check": "String"
+        }
+      ],
+      "output": "Number",
+      "colour": 160,
+      "tooltip": "Returns number of letters in the provided text.",
+      "helpUrl": "http://www.w3schools.com/jsref/jsref_length_string.asp"
+    });
+  }
+};
