fb88c1b7121c3c11499a7ed88d3730175f0390e4
Zody Woolsey@greenbush <52427035+zwoolsey-gbush@users.noreply.github.com>
2019-12-06 15:54:19 -0600

    update: interaction with ammo.js

---
+++ primary.js
@@ -0,0 +1,30 @@
+let express = require('express');
+let path = require('path');
+let app = express();
+let port = 80;
+app.get('/', (req, res) => res.sendFile( path.join(__dirname,'index.html')));
+
+app.listen(port, () => console.log(`VR running on port ${port}`));
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+    }
+    ++alias;
+  });
+});
